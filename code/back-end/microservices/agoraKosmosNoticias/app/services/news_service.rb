ceabc001fc9a618ff99a1092c1b4c2c4164dadd0
ricardo frança soares <rickfsoaresdev@gmail.com>
2024-08-12 01:35:40 -0300

    news service created but not implemented

---
+++ code/back-end/microservices/agoraKosmosNoticias/app/services/news_service.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+require 'news_services_pb'
+
+class NewsService < News::NewsService::Service
+  def get_news(_request, _call)
+    # todo    
+  end
+
+  def get_news_by_topic(_request, _call)
+    #todo
+  end
