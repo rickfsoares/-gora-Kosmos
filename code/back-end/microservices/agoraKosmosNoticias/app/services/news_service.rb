8e58072985a90133e688704120e7c390ca01d5a8
ricardo frança soares <rickfsoaresdev@gmail.com>
2024-08-13 01:34:37 -0300

    news service implemented but not tested

--- code/back-end/microservices/agoraKosmosNoticias/app/services/news_service.rb
+++ code/back-end/microservices/agoraKosmosNoticias/app/services/news_service.rb
@@ -1,11 +1,52 @@
 # frozen_string_literal: true
 require 'news_services_pb'
+require 'http'
 
 class NewsService < News::NewsService::Service
-  def get_news(_request, _call)
-    # todo    
+  def get_news(request, _call)
+    api_key = ENV['ALPHA_VANTAGE_API_KEY']
+    url = "https://www.alphavantage.co/query?function=NEWS_SENTIMENT&apikey=#{api_key}&page=#{request.page_number}&per_page=#{request.resuts_per_page}"
+
+    response = HTTP.get(url)
+
+    json_data = JSON.parse(response.body)
+
+    news_items = json_data['articles'].map do |article|
+      NewsItem.new(
+        title: article['title'],
+        summary: article['summary'],
+        url: article['url'],
+        banner_image: article['banner_image'],
+        topics: article['topics']
+      )
+    end
+
+    NewsResponse.new(news: news_items)
   end
 
-  def get_news_by_topic(_request, _call)
-    #todo
+  def get_news_by_topic(request, _call)
+    api_key = ENV['ALPHA_VANTAGE_API_KEY']
+
+    if request.topics.empty?
+      raise GRPC::InvalidArgument, "O parâmetro 'topics' é obrigatório"
+    end
+
+    url = "https://www.alphavantage.co/query?function=NEWS_SENTIMENT&apikey=#{api_key}&topics=#{request.topics}&page=#{request.page_number}&per_page=#{request.resuts_per_page}"
+
+    response = HTTP.get(url)
+
+    json_data = JSON.parse(response.body)
+
+    news_items = json_data['articles'].map do |article|
+      NewsItem.new(
+        title: article['title'],
+        summary: article['summary'],
+        url: article['url'],
+        banner_image: article['banner_image'],
+        topics: article['topics']
+      )
+    end
+
+    NewsResponse.new(news: news_items)
+
   end
