200514eff44d19a4d3e569143f50715980388b16
jonasapm <jonasapm94@gmail.com>
2024-08-16 17:18:31 -0300

    criação da listagem de ativos na página de invest

--- code/front-end/kosmos_frontend/src/main.ts
+++ code/front-end/kosmos_frontend/src/main.ts
@@ -4,9 +4,11 @@ import { importProvidersFrom } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { routes } from './app/app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    importProvidersFrom(BrowserModule, RouterModule.forRoot(routes))
+    importProvidersFrom(BrowserModule, RouterModule.forRoot(routes)),
+    provideAnimations()
   ]
 }).catch(err => console.error(err));
