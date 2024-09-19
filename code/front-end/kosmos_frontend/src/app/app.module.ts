96c96fb9c9741e84a10da72a3787a3190cb48904
ricardo frança soares <rickfsoaresdev@gmail.com>
2024-09-19 14:04:29 -0300

    interptor added

--- code/front-end/kosmos_frontend/src/app/app.module.ts
+++ code/front-end/kosmos_frontend/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgApexchartsModule } from 'ng-apexcharts';
+import { InterceptorModule } from './shared/interceptor.module';
 
 @NgModule({
   declarations: [
   ],
   imports: [
+    InterceptorModule,
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
