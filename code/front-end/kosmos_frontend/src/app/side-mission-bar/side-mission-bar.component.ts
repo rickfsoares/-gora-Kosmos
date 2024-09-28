b7fe8c54464cffc2ba0f02f7bb01f8700b4bb5ea
ricardo frança soares <rickfsoaresdev@gmail.com>
2024-09-28 09:56:05 -0300

    mission mock

--- code/front-end/kosmos_frontend/src/app/side-mission-bar/side-mission-bar.component.ts
+++ code/front-end/kosmos_frontend/src/app/side-mission-bar/side-mission-bar.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import {MatExpansionModule} from '@angular/material/expansion';
+import { Mission } from '../models/mission';
 
 @Component({
   selector: 'app-side-mission-bar',
@@ -10,5 +11,10 @@ import {MatExpansionModule} from '@angular/material/expansion';
   styleUrl: './side-mission-bar.component.scss'
 })
 export class SideMissionBarComponent {
+  missions: Array<Mission> = [
+    new Mission("missão 1", "compre 2 ativos", 2),
+    new Mission("missão 2", "compre 4 ativos", 2)
+  ];
+
   readonly panelOpenState = signal(false);
 }
