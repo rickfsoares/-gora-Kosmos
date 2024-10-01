84b0e4a3d29d0a60d5c80132961d6df4aa6e4312
tawhamjavascript <taw.ham.balbino9914@gmail.com>
2024-10-01 17:27:09 -0300

    testes

--- code/back-end/microservices/agoraKosmos/test/models/user_mission_test.rb
+++ code/back-end/microservices/agoraKosmos/test/models/user_mission_test.rb
@@ -4,4 +4,21 @@ class UserMissionTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user_level1 = users(:ricardo)
+    @level1 = levels(:level1)
+    @mission1 = missions(:missao1)
+    @mission2 = missions(:missao2)
+
+    @user_level1.missions << @mission1
+    @user_level1.missions << @mission2
+
+  
+
+  end 
+
+  test "user should access only incomplete missions" do
+    missions = UserMission.where(user: @user_level1).map(&:mission)
+    assert_not_equal missions, @user_level1.incomplete_missions
+  end
 end
