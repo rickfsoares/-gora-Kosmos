d251978ef6b88200a0ebed3881e699f6024299d8
Jonas Ariel <jonasapm@gmail.com>
2024-10-02 10:03:39 -0300

    apresentar a máscara quando o objeto vem do backend

--- code/front-end/kosmos_frontend/src/app/shared/cep.directive.ts
+++ code/front-end/kosmos_frontend/src/app/shared/cep.directive.ts
@@ -1,34 +1,61 @@
-import { Directive, ElementRef, HostListener, OnInit } from '@angular/core';
+import { Directive, ElementRef, forwardRef, HostListener } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Directive({
   selector: '[appCep]',
-  standalone: true
+  standalone: true,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => CepDirective),
+      multi: true
+    }
+  ]
 })
-export class CepDirective  implements OnInit{
+export class CepDirective implements ControlValueAccessor {
 
   constructor(private el: ElementRef) { }
 
-  ngOnInit(): void {
-    this.applyMask(this.el.nativeElement.value)
+  private onChange: (value: string) => void = () => {};
+  private onTouched: () => void = () => {};
+
+  writeValue(value: string): void {
+    if (value) {
+      this.el.nativeElement.value = this.applyMask(value);
+    }
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.el.nativeElement.disabled = isDisabled;
   }
 
   @HostListener('input', ['$event'])
-  onInputChange(event: Event){
-    const input = this.el.nativeElement
-    this.applyMask(input.value)
+  onInputChange(event: Event): void {
+    const input = this.el.nativeElement.value;
+    const maskedValue = this.applyMask(input);
+    this.el.nativeElement.value = maskedValue;
+    this.onChange(maskedValue); // Update the model with the masked value
   }
 
-  private applyMask(value: string){
-    const input = this.el.nativeElement;
-   value = input.value.replace(/\D/g, '');
+  private applyMask(value: string): string {
+    // Remove non-digit characters
+    value = value.replace(/\D/g, '');
 
-    if (value.length > 2 && value.length <= 5){
-      value = `${value.slice(0,2)}.${value.slice(2)}`
-    } else if (value.length > 5){
-      value = `${value.slice(0,2)}.${value.slice(2,5)}-${value.slice(5,8)}`
+    // Apply CEP mask
+    if (value.length > 2 && value.length <= 5) {
+      value = `${value.slice(0, 2)}.${value.slice(2)}`;
+    } else if (value.length > 5) {
+      value = `${value.slice(0, 2)}.${value.slice(2, 5)}-${value.slice(5, 8)}`;
     }
 
-    input.value = value
+    return value;
   }
-
 }
