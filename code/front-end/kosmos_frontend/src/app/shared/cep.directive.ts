33e4331ad0f1a79f04398d72f3f5f9d26262c231
Jonas Ariel <jonasapm@gmail.com>
2024-10-02 09:59:17 -0300

    diretiva de cep sendo inicializada quando recebe dados do backend

--- code/front-end/kosmos_frontend/src/app/shared/cep.directive.ts
+++ code/front-end/kosmos_frontend/src/app/shared/cep.directive.ts
@@ -1,17 +1,26 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appCep]',
   standalone: true
 })
-export class CepDirective  {
+export class CepDirective  implements OnInit{
 
   constructor(private el: ElementRef) { }
 
+  ngOnInit(): void {
+    this.applyMask(this.el.nativeElement.value)
+  }
+
   @HostListener('input', ['$event'])
   onInputChange(event: Event){
+    const input = this.el.nativeElement
+    this.applyMask(input.value)
+  }
+
+  private applyMask(value: string){
     const input = this.el.nativeElement;
-    let value = input.value.replace(/\D/g, '');
+   value = input.value.replace(/\D/g, '');
 
     if (value.length > 2 && value.length <= 5){
       value = `${value.slice(0,2)}.${value.slice(2)}`
